d6a3d95b96c8271d16ea655c3205f828f158a669
bask01 <bask@sheridancollege.ca>
2022-01-21 13:18:09 -0500

    First commit

---
+++ src/main/java/ca/sheridancollege/bask/as1/A1BaskApplication.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.bask.as1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A1BaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A1BaskApplication.class, args);
+	}
+
+}
